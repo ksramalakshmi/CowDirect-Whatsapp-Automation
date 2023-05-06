1ab795d0857231ed9b7045882af8f21efb0268ab
K S Ramalakshmi <rlsri2305@gmail.com>
2023-05-06 22:25:07 +0530

    first commit

---
+++ message1.py
@@ -0,0 +1,97 @@
+from pywhatkit import sendwhatmsg_instantly as sendwhatmsg
+import pandas as pd
+from datetime import date, datetime
+
+data = pd.read_csv("sample.csv")
+
+today = date.today()
+today = today.strftime("%d/%m/%Y")
+
+for index, row in data.iterrows():
+    if row['Status'] != 'Inactive':
+        milk_supplied = str(row['Milk supply'])
+        payment_received = str(row['This month payment received'])
+        previous_month = row['Previous month pending bill']
+        cost_of_milk = str(row['Milk sale value'])
+        remaining_balance = row['Remaining to pay']
+
+        message= """"""
+        if remaining_balance < 0 and previous_month < 0:
+            message="""
+Dear Customer,
+
+Please find the milk a/c from 01/04/2023 to """ + today + """.
+
+Milk supplied:""" + milk_supplied + """ Ltrs
+Supplied milk cost: Rs.""" + cost_of_milk + """
+Paid this month: Rs.""" + payment_received + """
+Last month advance: Rs.""" + str(previous_month).strip('-') + """
+Balance excess: Rs.""" + str(remaining_balance).strip('-') + """
+
+Assuring our services,
+Team CowDirect
+                    """
+        elif remaining_balance < 0 and previous_month > 0:
+            message="""
+Dear Customer,
+
+Please find the milk a/c from 01/04/2023 to """ + today + """.
+
+Milk supplied:""" + milk_supplied + """ Ltrs
+Supplied milk cost: Rs.""" + cost_of_milk + """
+Paid this month: Rs.""" + payment_received + """
+Last month due: Rs.""" + str(previous_month) + """
+Balance excess: Rs.""" + str(remaining_balance).strip('-') + """
+
+Assuring our services,
+Team CowDirect
+                    """
+            
+        elif remaining_balance > 0 and previous_month > 0:
+            message="""
+Dear Customer,
+
+Please find the milk a/c from 01/04/2023 to """ + today + """.
+
+Milk supplied: """ + milk_supplied + """ Ltrs
+Supplied milk cost : Rs.""" + cost_of_milk + """
+Paid this month: Rs.""" + payment_received + """
+Last month due: Rs.""" + str(previous_month) + """
+*Balance due: Rs.""" + str(remaining_balance) + """*
+
+*Kindly make the payment*,
+
+Assuring our services,
+Team CowDirect
+                    """
+
+        elif remaining_balance > 0 and previous_month < 0:
+            message="""
+Dear Customer,
+
+Please find the milk a/c from 01/04/2023 to """ + today + """.
+
+Milk supplied: """ + milk_supplied + """ Ltrs
+Supplied milk cost : Rs.""" + cost_of_milk + """
+Paid this month: Rs.""" + payment_received + """
+Last month advance: Rs.""" + str(previous_month).strip('-') + """
+*Balance due: Rs.""" + str(remaining_balance) + """*
+
+*Kindly make the payment*,
+
+Assuring our services,
+Team CowDirect
+                    """
+
+        phone_no = "+91" + str(row['Phone number'])
+
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S")
+        current_time = current_time.split(":")
+        current_time = [int(i) for i in current_time]
+
+        sendwhatmsg(phone_no, message, tab_close=True)
+
+        print(message)
+        print("---------------------------------------------------")
+print("All messages sent successfully!")
