89ab98f9300c59f7da2eaec5ae2dc2b058d25312
K S Ramalakshmi <rlsri2305@gmail.com>
2024-07-09 11:47:40 +0530

    Update message1.py
    Soles version

--- message1.py
+++ message1.py
@@ -4,8 +4,8 @@ from datetime import date, datetime
 
 data = pd.read_csv("sample.csv")
 
-if len(data.columns) != 10:
-    print("Please check the file. It should have 10 columns.")
+if len(data.columns) != 2:
+    print("Please check the file. It should have 2 columns.")
     exit()
 
 today = date.today()
@@ -15,90 +15,23 @@ first_day = date.today().replace(day=1)
 first_day = first_day.strftime('%d %b %Y')
 
 for index, row in data.iterrows():
-    if row['Status'] != 'Inactive':
-        milk_supplied = str(row['Milk supply'])
-        payment_received = str(row['This month payment received'])
-        previous_month = row['Previous month pending bill']
-        cost_of_milk = str(row['Milk sale value'])
-        remaining_balance = row['Remaining to pay']
+    name = str(row['Name'])
+    message= """
+Dear """ + name + """,
+SOLES IS BACK !!! ✨ 
+We're excited to announce the grand launch of our new store at Bangalore's favourite shopping destination- Commercial Street. 🎆 
+Come visit us at our new store and enjoy your  shopping experience with our latest collection of  women's footwear
+"""
+       
+    phone_no = "+91" + str(row['Phone number'])
 
-        message= """"""
-        if remaining_balance < 0 and previous_month <= 0:
-            message="""
-Dear Customer,
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    current_time = current_time.split(":")
+    current_time = [int(i) for i in current_time]
 
-Milk account from """+ first_day +""" to """ + today + """.
+    sendwhatmsg(phone_no, message, tab_close=True, wait_time=20)
 
-Milk supplied:""" + milk_supplied + """ Ltrs
-Supplied milk cost: ₹ """ + cost_of_milk + """
-Paid this month: ₹ """ + payment_received + """
-Last month advance: ₹ """ + str(previous_month).strip('-') + """
-Balance excess: ₹ """ + str(remaining_balance).strip('-') + """
-
-Enjoy Natural Cow Milk Daily,
-Team CowDirect
-                    """
-        elif remaining_balance < 0 and previous_month > 0:
-            message="""
-Dear Customer,
-
-Milk account from """+ first_day +""" to """ + today + """.
-
-Milk supplied:""" + milk_supplied + """ Ltrs
-Supplied milk cost: ₹ """ + cost_of_milk + """
-Paid this month: ₹ """ + payment_received + """
-Last month due: ₹ """ + str(previous_month) + """
-Balance excess: ₹ """ + str(remaining_balance).strip('-') + """
-
-Enjoy Natural Cow Milk Daily,
-Team CowDirect
-                    """
-            
-        elif remaining_balance > 0 and previous_month > 0:
-            message="""
-Dear Customer,
-
-Milk account from """+ first_day +""" to """ + today + """.
-
-Milk supplied: """ + milk_supplied + """ Ltrs
-Supplied milk cost : ₹ """ + cost_of_milk + """
-Paid this month: ₹ """ + payment_received + """
-Last month due: ₹ """ + str(previous_month) + """
-*Balance due: ₹ """ + str(remaining_balance) + """*
-
-*Kindly pay ₹ """ + str(remaining_balance) + """ to 9036543335 through Gpay or Paytm or Phonepe.*,
-
-Enjoy Natural Cow Milk Daily,
-Team CowDirect
-                    """
-
-        elif remaining_balance > 0 and previous_month <= 0:
-            message="""
-Dear Customer,
-
-Milk account from """+ first_day +""" to """ + today + """.
-
-Milk supplied: """ + milk_supplied + """ Ltrs
-Supplied milk cost : ₹ """ + cost_of_milk + """
-Paid this month: ₹ """ + payment_received + """
-Last month advance: ₹ """ + str(previous_month).strip('-') + """
-*Balance due: ₹ """ + str(remaining_balance) + """*
-
-*Kindly pay ₹ """ + str(remaining_balance) + """ to 9036543335 through Gpay or Paytm or Phonepe.*,
-
-Enjoy Natural Cow Milk Daily,
-Team CowDirect
-                    """
-
-        phone_no = "+91" + str(row['Phone number'])
-
-        now = datetime.now()
-        current_time = now.strftime("%H:%M:%S")
-        current_time = current_time.split(":")
-        current_time = [int(i) for i in current_time]
-
-        sendwhatmsg(phone_no, message, tab_close=True, wait_time=20)
-
-        print(message)
-        print("---------------------------------------------------")
+    print(message)
+    print("---------------------------------------------------")
 print("All messages sent successfully!")
